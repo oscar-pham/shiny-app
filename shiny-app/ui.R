1283167c9dd6512f0ee91eb9c47e3f5978e1de59
Oscar Pham <tpha0236@uni.sydney.edu.au>
2024-09-16 17:49:38 +1000

    Completed t-Test function
    Finished up the hypothesis testing tab

--- shiny-app/ui.R
+++ shiny-app/ui.R
@@ -1,6 +1,8 @@
 library(shiny)
 library(ggplot2)
 library(DT)
+library(sortable)
+library(shinyWidgets)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -21,10 +23,9 @@ ui <- fluidPage(
                    choices = list(
                      "Chi-Squared Test for Independence" = 1,
                      "Chi-Squared Goodness of Fit Test" = 2,
-                     "One-sample t-test" = 3,
-                     "Two-sample t-test" = 4
+                     "Two-sample t-test" = 3
                    ),
-                   selected = 1
+                   selected = 3
                  ),
                  
                  # These UI elements will dynamically display when test 1 (Chi-Squared Test for Independence) is selected
@@ -44,6 +45,36 @@ ui <- fluidPage(
                      textInput("expected_values", "Enter expected proportions (comma-separated)", "0.25, 0.25, 0.25, 0.25")
                    )
                  ),
+                 conditionalPanel(
+                   condition = "input.select == 3",
+                   # Dropdown to select the quantitative variable
+                   selectInput(
+                     inputId = "quantitative_var",
+                     label = "Select a Quantitative Variable:",
+                     choices = NULL
+                   ),
+                   
+                   # Dropdown to select the categorical variable
+                   selectInput(
+                     inputId = "categorical_var",
+                     label = "Select a Categorical Variable:",
+                     choices = NULL
+                   ),
+                   
+                   switchInput(inputId = "perm", label = "Permutation", value = FALSE, size = "small"),
+                   switchInput(inputId = "welch", label = "Welch", value = FALSE, size = "small"),
+                   
+                   # Instructions for users
+                   h4("Drag and drop categories into Sample 1 and Sample 2 boxes"),
+                   
+                   # Drag-and-drop containers for Sample 1 and Sample 2
+                   fluidRow(
+                     uiOutput("bucket_list_ui")
+                   )
+                 ),
+                 
+                 
+                 
                  # Slider for selecting significance level (alpha)
                  sliderInput("alpha", "Significance Level (α)", min = 0.01, max = 0.1, value = 0.05, step = 0.01)
                ),
@@ -63,7 +94,7 @@ ui <- fluidPage(
                      h3("Hypothesis:"),
                      p(uiOutput("dynamic_hypothesis")),
                      h3("Assumptions:"),
-                     p("We assume that all observations are independent. To confirm this assumption, we will check whether all expected cell counts are greater than or equal to 5. This will be done by creating a contingency table for the expected cell counts of the two selected variables."),
+                     p("We assume that all observations are independent. We will also assume that all expected cell counts are at least 5. This assumption can be checked by creating a contingency table for the expected cell counts of the selected variable."),
                      DT::dataTableOutput("expected_table"),
                      br(),
                      uiOutput("warning_message"),
@@ -95,7 +126,7 @@ ui <- fluidPage(
                    h3("Hypothesis:"),
                    p(uiOutput("gof_hypothesis")),
                    h3("Assumptions:"),
-                   p("We assume that all observations are independent. To confirm this assumption, we will check whether all expected cell counts are greater than or equal to 5. This will be done by creating a contingency table for the expected cell counts of the selected variable."),
+                   p("We assume that all observations are independent. We will also assume that all expected cell counts are at least 5. This assumption can be checked by creating a contingency table for the expected cell counts of the selected variable."),
                    h4("Expected vs. Observed:"),
                    DT::dataTableOutput("gof_table"),
                    uiOutput("warning_message"),
@@ -106,6 +137,39 @@ ui <- fluidPage(
                    uiOutput("gof_p_result"),
                    h3("Conclusion:"),
                    uiOutput("gof_conclusion")
+                 ),
+                 conditionalPanel(
+                   condition = "input.select == 3",
+                   h1("Two-sample t-Test"),
+                   
+                   # Check if the correct number of samples are selected
+                   conditionalPanel(
+                     condition = "input.rank_list_1 && input.rank_list_2 && (input.rank_list_1.length != 1 || input.rank_list_2.length != 1)",
+                     br(),
+                     h4("Please select exactly one sample for each group. No more, no less.")
+                   ),
+                   
+                   # Render the rest only if exactly one sample is selected per group
+                   conditionalPanel(
+                     condition = "input.rank_list_1 && input.rank_list_2 && (input.rank_list_1.length == 1 && input.rank_list_2.length == 1)",
+                   h3("Hypothesis:"),
+                   p(uiOutput("dynamic_hypothesis_ttest")),
+                   h3("Assumptions:"),
+                   p("We assume that all observations are independent. We assume the data in each group is approximately normal and the groups have equal variance. To check normality, we will use Q-Q plots for each sample, and to check for equal variance, we will use side-by-side box plots."),
+                   h4("Q-Q plot"),
+                   p("In a Q-Q plot, if the data points closely follow the red dotted line, it indicates that the data is approximately normally distributed. Deviations from this line suggest that the data may not follow a normal distribution, with systematic patterns like curves indicating skewness and outliers appearing as points far from the line. If normality is violated, consider enabling the 'Permutation' option to run a permutation test."),
+                   plotOutput("qq_plot"),
+                   h4("Box plot"),
+                   p(" In a box plot, if the boxes (interquartile ranges) for two groups are of similar length, it suggests that the groups may have equal variances. Significant differences in box height imply unequal variances, and outliers are shown as individual points beyond the whiskers. If unequal variances are detected, consider enabling the 'Welch' option to perform a Welch's t-test."),
+                   plotOutput("box_plot"),
+                   h3("Test statistic:"),
+                   uiOutput("t_t_result"),
+                   h3("P-value:"),
+                   uiOutput("t_p_result"),
+                   h3("Conclusion:"),
+                   uiOutput("t_conclusion")
+                   
+                  )
                  )
                )
              )
